afb15cf809b9154110cd4b3be4694287f4c95108
sousa-gabriel <44657244+sousa-gabriel@users.noreply.github.com>
2022-09-16 08:42:35 -0300

    insert API fake this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./sytles/global";
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
