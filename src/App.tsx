cf276a0369068db340c4c9760db6d61681a9c93c
sousa-gabriel <44657244+sousa-gabriel@users.noreply.github.com>
2022-09-21 23:26:29 -0300

    finish Modal New Transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,51 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./sytles/global";
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
+import { useState } from "react";
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        }
-      ]
+      return this.schema.all('transaction')
     })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+
   }
 })
 
+
+
 export function App() {
+  const [isNewTransactionModal, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransactionModal isOpen={isNewTransactionModal} onRequestClose={handleCloseNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import Logo from '../../assets/logo.svg';
 import * as S from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <S.Container>
         <S.Content>
             <img src={Logo} alt="dt money" />
-            <button type="button">
+            <button type="button" onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
         </S.Content>
